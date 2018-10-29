64b9274694f04ea6c61e418339ad384c17b1c2e3
dbhaig <time4tux@gmail.com>
2018-10-28 21:15:00 -0400

    Modified IRLib_P14_RCA.h to use generic decode function.

--- IRLibProtocols/IRLib_P14_RCA.h
+++ IRLibProtocols/IRLib_P14_RCA.h
@@ -50,54 +50,12 @@ class IRdecodeRCA: public virtual IRdecodeBase {
             return RAW_COUNT_ERROR;
         }
 
-        if ( !ignoreHeader && (recvGlobal.decodeLength==52) ){
-            // Ignore Header Mark length
-            if (!MATCH(recvGlobal.decodeBuffer[1],RCA_HEAD_SPACE)) {
-                return HEADER_SPACE_ERROR(RCA_HEAD_SPACE);
-            }
-            offset=3;
-        } else {
-            // Ignore Header Space length
-            if (!MATCH(recvGlobal.decodeBuffer[1],RCA_DATA_MARK)) {
-                return DATA_MARK_ERROR(RCA_DATA_MARK);
-            }
-            offset=1;
-        }
-        int i = offset;
-        while (i < recvGlobal.decodeLength-1) {
-            i++;
-        }
+        if ( (!decodeGeneric(52, RCA_HEAD_MARK, RCA_HEAD_SPACE, RCA_DATA_MARK , RCA_ONE, RCA_ZERO)) &&
+             (!decodeGeneric(50, 0, 0, RCA_DATA_MARK , RCA_ONE, RCA_ZERO)) ) return false;
 
-        uint32_t data=0;
-        while (offset < (recvGlobal.decodeLength-1)) {
-            if (!ABS_MATCH(recvGlobal.decodeBuffer[offset],RCA_DATA_MARK,RCA_TOLERANCE)){
-                    return DATA_MARK_ERROR(RCA_DATA_MARK);
-            }
-            offset++;
-          
-            if (ABS_MATCH(recvGlobal.decodeBuffer[offset],RCA_ZERO, RCA_TOLERANCE) ) { //Logical "0"
-                data <<= 1;
-            } else if (ABS_MATCH(recvGlobal.decodeBuffer[offset],RCA_ONE, RCA_TOLERANCE) ) { //Logical "1"
-                        data = (data<<1) + 1;
-            } else {
-                    return DATA_SPACE_ERROR(RCA_ZERO);
-            }
-            offset++;
-        }
-
-        // Look for stop bit
-        if (!MATCH(recvGlobal.decodeBuffer[offset],RCA_DATA_MARK)){
-            return DATA_MARK_ERROR(RCA_DATA_MARK);
-        }
-
-        bits = recvGlobal.decodeLength-4;//set bit length
-        value = data;//put remaining bits in value
         protocolNum=RCA;
         return true;
     }
 };
 #endif //IRLIBDECODEBASE_H
-
-#define IRLIB_HAVE_COMBO
-
-#endif //IRLIB_PROTOCOL_14_H
+#endif IRLIB_PROTOCOL_14_H
